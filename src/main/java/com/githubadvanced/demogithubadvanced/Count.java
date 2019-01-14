f06a5cde898cd1b6cac836380b299d4d8c8805b5
Zoli <mr.zoltan.takacs@gmail.com>
2019-01-14 14:38:17 +0100

    a3.1

--- src/main/java/com/githubadvanced/demogithubadvanced/Count.java
+++ src/main/java/com/githubadvanced/demogithubadvanced/Count.java
@@ -2,8 +2,8 @@ package com.githubadvanced.demogithubadvanced;
 
 public class Count {
     public void count(Integer n){
-        n = 13;
-        
+        n = 10;
+
         System.out.println("My age is " + n);
         System.out.println("shit");
     }
