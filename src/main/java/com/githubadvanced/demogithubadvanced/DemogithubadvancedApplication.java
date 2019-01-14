d8a60ee336a9a93dec2bbc957523df2758db282a
Gilthanas122 <pityu.django@gmail.com>
2019-01-14 13:11:49 +0100

    initialise project

---
+++ src/main/java/com/githubadvanced/demogithubadvanced/DemogithubadvancedApplication.java
@@ -0,0 +1,14 @@
+package com.githubadvanced.demogithubadvanced;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemogithubadvancedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemogithubadvancedApplication.class, args);
+    }
+
+}
+
