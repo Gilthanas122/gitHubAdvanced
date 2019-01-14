39ea67ef1a0a95a8ca43653d885237f15e6ec8ab
Zoli <mr.zoltan.takacs@gmail.com>
2019-01-14 13:29:59 +0100

    a1

---
+++ src/main/java/com/githubadvanced/demogithubadvanced/greet.java
@@ -0,0 +1,11 @@
+package com.githubadvanced.demogithubadvanced;
+
+import javax.print.attribute.standard.PrinterName;
+
+public class greet {
+
+    public static void greet(){
+        System.out.println("Zoli");
+    }
+
+}
