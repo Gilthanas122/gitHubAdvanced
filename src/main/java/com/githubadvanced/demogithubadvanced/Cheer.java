42defe519536a93d1a7144667954e1bb3c879ca0
Peter Ripka <ripka.peter@gmail.com>
2019-01-14 13:49:18 +0100

    c2

--- src/main/java/com/githubadvanced/demogithubadvanced/Cheer.java
+++ src/main/java/com/githubadvanced/demogithubadvanced/Cheer.java
@@ -7,7 +7,7 @@ public class Cheer {
 
     }
 
-    public void cheer2(String text) {
+    public void cheer2() {
         String teamName = "gitHubadvanced";
         System.out.println( "Our teamname is " + teamName );
 
