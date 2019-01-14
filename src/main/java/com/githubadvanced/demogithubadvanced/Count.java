06d21991b93986cc78b074d4317569dd3d5cae11
Peter Ripka <ripka.peter@gmail.com>
2019-01-14 14:56:04 +0100

    c6

--- src/main/java/com/githubadvanced/demogithubadvanced/Count.java
+++ src/main/java/com/githubadvanced/demogithubadvanced/Count.java
@@ -4,7 +4,7 @@ public class Count {
     public void count(Integer n){
         n = 10;
 
-        System.out.println("My age is " + n);
+        System.out.println("Counting the countless " + n);
         System.out.println("shit");
     }
 }
