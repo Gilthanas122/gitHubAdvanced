418be2735af7f20ba758390054675f1df91647a0
Peter Ripka <ripka.peter@gmail.com>
2019-01-14 13:38:57 +0100

    c1

---
+++ src/main/java/com/githubadvanced/demogithubadvanced/Cheer.java
@@ -0,0 +1,12 @@
+package com.githubadvanced.demogithubadvanced;
+
+
+public class Cheer {
+
+    public static void main(String[] args) {
+
+    }
+    public void cheer(){
+        System.out.println("gitHubAdvanced");
+    }
+}
